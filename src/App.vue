a85ef88c6bc9dfcfda04f19aca41f65367d31e3a
Mauricio Maldonado Iturriago <mauriciomaldo@hotmail.es>
2020-03-18 00:10:15 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<barcode :value="value" :tag="tag"  :options="options"></barcode>
+
+</template>
+<script>
+export default {
+ data () {
+    return {
+        value: 'TEST',
+        tag: 'svg',
+        options: {
+            lineColor: '#ff7069',
+            fontSize: 32,
+            font: 'Courier',
+            width: 3,
+            height: 60,
+            marginBottom: 60,
+            format: 'CODE 39',
+            background: '#ccffff'
+        },
+    }
+}
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueBarcode from 'vue-barcode'
+
+Vue.component('barcode', VueBarcode)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
